e96de8c79c59efe65796e743c42beb045f9df8d0
danielancines <daniel.ancines@gmail.com>
2018-06-26 01:34:24 -0300

    Routes config to use on Ubuntu Server

---
+++ src/config/pg.js
@@ -0,0 +1,9 @@
+const user = ''
+const host = ''
+const database = ''
+const password = ''
+const port = 5432
+
+module.exports = {
+    connectionString: 'postgresql://dbuser:secretpassword@database.server.com:3211/mydb'
+}
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ const port = process.env.port || 3000
 const bodyParser = require('body-parser')
 const apiV1 = require('./routes/v1/routes')
 
-app.use('/api/v1', apiV1)
+app.use('/tvshow/api/v1', apiV1)
 
 const server = app.listen(port, () => {
     console.log(`Server running at ${server.address().address} and listen port: ${server.address().port}`)
